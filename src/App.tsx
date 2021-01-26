af8294c87e1798a8d979d98c21a02a1e8399f9ee
Maximilian Stümpfl <maximilian2304@t-online.de>
2021-01-26 17:19:32 +0100

    Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,46 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+    <div className='text-center'>
+      <header className='App-header min-h-screen flex flex-col items-center justify-center text-white'>
+        <img src={logo} className='h-40 App-logo' alt='logo' />
+        <p className='mb-5'>Hello Vite + React + Typescript + Electron</p>
         <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+          <button
+            className='border border-solid border-white rounded-lg p-3 hover:bg-yellow-500 outline-none ring-0'
+            onClick={() => setCount((count) => count + 1)}
+          >
+            Press
           </button>
         </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
+        <p className='mt-7'>count is: {count}</p>
         <p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='text-gray-600'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Learn React
           </a>
           {' | '}
           <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='text-gray-600'
+            href='https://vitejs.dev/guide/features.html'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Vite Docs
           </a>
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
