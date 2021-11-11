eacb46b93b28cbd65b1f375f5d3b2fd08525e393
Rizky Tri Sulistyo <rizky@ids.co.id>
2021-11-12 01:49:01 +0700

    fix maximize toggle

--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Icon from './Icon-Electron.png';
 
 export default function AppBar() {
+  const [isMaximize, setMaximize] = useState(false);
+
+  const handleToggle = () => {
+    if (isMaximize) {
+      setMaximize(false);
+    } else {
+      setMaximize(true);
+    }
+    window.Main.Maximize();
+  };
+
   return (
     <>
       <div className="py-0.5 flex justify-between draggable">
         <div className="inline-flex">
-          <img className="h-6" src={Icon} alt="Icon of Electron" />
-          <p className="text-xs pt-1 -ml-1">Vite App</p>
+          <img className="h-6 lg:-ml-2" src={Icon} alt="Icon of Electron" />
+          <p className="text-xs md:pt-1 md:-ml-1 lg:-ml-2">Vite App</p>
         </div>
         <div className="inline-flex -mt-1">
-          <button onClick={window.Main.Minimize} className="undraggable px-4 pt-1 hover:bg-gray-300">
+          <button onClick={window.Main.Minimize} className="undraggable md:px-4 lg:px-3 pt-1 hover:bg-gray-300">
             &#8211;
           </button>
-          <button onClick={window.Main.Maximize} className="undraggable px-6 pt-1 hover:bg-gray-300">
-            &#8414;
+          <button onClick={handleToggle} className="undraggable px-6 lg:px-5 pt-1 hover:bg-gray-300">
+            {isMaximize ? '\u2752' : '⃞'}
           </button>
           <button onClick={window.Main.Close} className="undraggable px-4 pt-1 hover:bg-red-500 hover:text-white">
             &#10005;
@@ -22,13 +34,13 @@ export default function AppBar() {
         </div>
       </div>
       <div className="bg-gray-900 text-white undraggable">
-        <ul className="flex text-center">
-          <li className=" text-sm w-8  hover:bg-gray-700">File</li>
-          <li className="text-sm w-8   hover:bg-gray-700">Edit</li>
-          <li className="text-sm w-10  hover:bg-gray-700">View</li>
-          <li className="text-sm w-14  hover:bg-gray-700 ">Window</li>
-          <li className="text-sm w-9  hover:bg-gray-700 ">Help</li>
-        </ul>
+        <div className="flex text-center">
+          <div className="text-sm w-8  hover:bg-gray-700">File</div>
+          <div className="text-sm w-8   hover:bg-gray-700">Edit</div>
+          <div className="text-sm w-10  hover:bg-gray-700">View</div>
+          <div className="text-sm w-14  hover:bg-gray-700 ">Window</div>
+          <div className="text-sm w-9  hover:bg-gray-700 ">Help</div>
+        </div>
       </div>
     </>
   );
