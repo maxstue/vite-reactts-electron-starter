e7e7dce1951c52aeaed2b0eaafef28da72ea66ff
Jawad <jhaider58@gmail.com>
2022-08-29 05:22:41 +0400

    bind selected asset to the charts and other coustomizations

--- src/charts/index.jsx
+++ src/charts/index.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import * as TradingView from "../public/charting_library";
 import Datafeed from './datafeed.js';
 import { useEffect } from "react";
+import { useGlobal } from "reactn";
+import {dev} from "./helpers";
+
 
 
 const overrides = {
@@ -9,21 +12,31 @@ const overrides = {
 }
 
 const Chart = function() {
-useEffect(() => {
-	var tvWidget = new TradingView.widget({
-		symbol: 'NASDAQ:AAPL', // default symbol
-		interval: '1D', // default interval
-		fullscreen: false, // displays the chart in the fullscreen mode
-		container: 'tv_chart_container',
-		datafeed: Datafeed,
-		library_path: '/charting_library/',
-		autosize: true,
-		// disabled_features: ["header_widget", "main_series_scale_menu", "display_market_status", "timeframes_toolbar", "control_bar"],
-		// enabled_features:["hide_left_toolbar_by_default", "hide_resolution_in_legend"],
-		overrides:overrides,
-		});
+	const [tvWidget, setTvWIdget] = useState();
+	const [selectedAsset] = useGlobal("selectedAsset");
+	useEffect(() => {
+		 setTvWIdget( new TradingView.widget({
+			symbol: selectedAsset == "" ? 'NASDAQ:AAPL' : `NASDAQ:	${selectedAsset}`, // default symbol
+			interval: '1D', // default interval
+			fullscreen: false, // displays the chart in the fullscreen mode
+			container: 'tv_chart_container',
+			datafeed: Datafeed,
+			library_path: '/charting_library/',
+			autosize: true,
+			disabled_features: ["header_symbol_search", "header_compare", "symbol_search_hot_key", "header_screenshot", "main_series_scale_menu", "display_market_status", "timeframes_toolbar", "control_bar"],
+			enabled_features:["hide_left_toolbar_by_default", "hide_resolution_in_legend"],
+			overrides:overrides,
+			}));
+		
+	}, [])
+	
+		useEffect(() => {
+			tvWidget?.onChartReady?.(()=>{
+				tvWidget?.activeChart?.().setSymbol?.(`NASDAQ:${selectedAsset}`);
+				if(dev) console.log("[onChartReady].[useEffect] selectedAsset changed to: ", selectedAsset);
+			});
+		}, [selectedAsset])
 
-}, [])
 	
     return (
 	<div id="tv_chart_container" className="w-full h-[88%]">
