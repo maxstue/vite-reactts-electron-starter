b0e60f93133bb6966a5e2650faeeb99fa95ed91a
Ronan-Yann Lorin <ryl@free.fr>
2022-09-01 19:23:22 +0200

    WIP

--- src/tape/index.jsx
+++ src/tape/index.jsx
@@ -8,24 +8,24 @@ import {
     Th,
     Td,
 } from "@chakra-ui/react";
-import { Tape } from "../../electron/ib/wrapper";
+// import { Tape } from "../../electron/ib/wrapper";
 
-type HookType = Tape[] | string | null;
+// type HookType = Tape[] | string | null;
 
-const rows: number = 8;                         // Number of rows to display
+const rows = 8;                         // Number of rows to display
 
-const TimeAndSales = (props: any) => {
+const TimeAndSales = (props) => {
 
-    const [selectedAsset] = useGlobal("selectedAsset" as never);
+    const [selectedAsset] = useGlobal("selectedAsset");
 
-    const [prices, setData] = React.useState(null as HookType);
+    const [prices, setData] = React.useState(null);
 
     const handleData = React.useCallback((data) => {
         console.log(data);
         if (data.symbol == selectedAsset) {
-            console.log("prices", prices);
+            // console.log("prices", prices);
             if (prices && (prices instanceof Array)) {
-                const tape: Tape = data.content;
+                const tape = data.content;
                 // for (let i = prices.length - 1; i > 0; --i) {
                 //     // console.log(i);
                 //     prices[i].ingressTm = prices[i - 1].ingressTm;
@@ -36,13 +36,13 @@ const TimeAndSales = (props: any) => {
                 // prices[0].price = tape.price;
                 // prices[0].size = tape.size;
 
-                // prices?.push(data.content);
-                // prices?.shift();
-                let newPrices: Tape[] = [];
-                newPrices.push(tape);
-                for (let i = 1; i < prices.length; i++) newPrices.push(prices[i]);
-                setData(newPrices);
-                console.log("newPrices", newPrices);
+                prices?.push(data.content);
+                // if (prices?.length > rows) prices?.shift();
+                // let newPrices = [];
+                // newPrices.push(tape);
+                // for (let i = 1; i < prices.length; i++) newPrices.push(prices[i]);
+                setData(prices);
+                // console.log("newPrices", prices);
             }
         } else if (data.error) {
             setData(data.error);
@@ -57,12 +57,12 @@ const TimeAndSales = (props: any) => {
 
     if (!prices) {
         console.log("init prices");
-        let prices: Tape[] = [];
-        for (let i = 0; i < rows; i++) prices.push({ ingressTm: i });
+        let prices = [];
+        // for (let i = 0; i < rows; i++) prices.push({ ingressTm: i });
         setData(prices);
     }
     if (typeof prices == "string") {
-        return (<p>Error: {prices}</p>);
+        return (<p>Error: { prices }</p>);
     } else if (selectedAsset) {
         return (
             <Table size="sm" colorScheme="gray">
@@ -74,12 +74,12 @@ const TimeAndSales = (props: any) => {
                     </Tr>
                 </Thead><Tbody>
                     {
-                        prices?.map((row: Tape, index: number) => {
+                        prices?.reverse().map((row, index) => {
                             return (
-                                <Tr key={index}>
-                                    <Td>{row.ingressTm}</Td>
-                                    <Td>{row.price}</Td>
-                                    <Td>{row.size}</Td>
+                                <Tr key={ index }>
+                                    <Td>{ row.ingressTm }</Td>
+                                    <Td>{ row.price }</Td>
+                                    <Td>{ row.size }</Td>
                                 </Tr>
                             );
                         })
