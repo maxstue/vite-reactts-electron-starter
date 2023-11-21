53158e792f48c624c9b7d5f1aa12a67dab901b87
Minf97 <287728237@qq.com>
2023-11-21 18:46:35 +0800

    antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import AppBar from './AppBar';
+import {Button} from "antd"
 
 function App() {
   console.log(window.ipcRenderer);
@@ -40,6 +41,7 @@ function App() {
           <AppBar />
         </div>
       )}
+      <Button type='primary'>Hello World</Button>
       <div className="flex-auto">
         <div className=" flex flex-col justify-center items-center h-full bg-gray-800 space-y-4">
           <h1 className="text-2xl text-gray-200">Vite + React + Typescript + Electron + Tailwind</h1>
