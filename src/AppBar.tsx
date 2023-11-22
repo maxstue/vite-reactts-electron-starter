4c327fcfbb4b0cd34ce18fa8e50d908264cd3346
Minf97 <287728237@qq.com>
2023-11-22 18:53:23 +0800

    appbar

--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 import Icon from './assets/icons/Icon-Electron.png';
+import { Button } from 'antd';
+import { HomeFilled } from '@ant-design/icons';
 
 function AppBar() {
   const [isMaximize, setMaximize] = useState(false);
@@ -16,30 +18,20 @@ function AppBar() {
 
   return (
     <>
-      <div className="py-0.5 flex justify-between draggable">
-        <div className="inline-flex">
-          <img className="h-6 lg:-ml-2" src={Icon} alt="Icon of Electron" />
-          <p className="text-xs md:pt-1 md:-ml-1 lg:-ml-2">Vite App</p>
+      <div className="flex draggable bg-white">
+        <div className="left flex flex-auto">
+          {/* <Button icon={<HomeFilled />} className="w-30px h-30px" type="text" /> */}
         </div>
-        <div className="inline-flex -mt-1">
-          <button onClick={window.Main.Minimize} className="undraggable md:px-4 lg:px-3 pt-1 hover:bg-gray-300">
+        <div className="right flex w-24 undraggable">
+          <Button onClick={window.Main.Minimize} className="w-8 flex-center" type="text">
             &#8211;
-          </button>
-          <button onClick={handleToggle} className="undraggable px-6 lg:px-5 pt-1 hover:bg-gray-300">
+          </Button>
+          <Button onClick={handleToggle} className="w-8 flex-center" type="text">
             {isMaximize ? '\u2752' : '⃞'}
-          </button>
-          <button onClick={window.Main.Close} className="undraggable px-4 pt-1 hover:bg-red-500 hover:text-white">
+          </Button>
+          <Button onClick={window.Main.Close} className="w-8 flex-center" type="text">
             &#10005;
-          </button>
-        </div>
-      </div>
-      <div className="bg-gray-900 text-white undraggable">
-        <div className="flex text-center">
-          <div className="text-sm w-8  hover:bg-gray-700">File</div>
-          <div className="text-sm w-8   hover:bg-gray-700">Edit</div>
-          <div className="text-sm w-10  hover:bg-gray-700">View</div>
-          <div className="text-sm w-14  hover:bg-gray-700 ">Window</div>
-          <div className="text-sm w-9  hover:bg-gray-700 ">Help</div>
+          </Button>
         </div>
       </div>
     </>
