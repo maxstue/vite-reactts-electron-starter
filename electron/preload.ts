b681d00536dcdb15e0c82d285d4197eb182eca0a
Alessandro B <ale_alessandro_ale@hotmail.com>
2024-07-05 18:16:07 -0300

    improvement in preload.ts

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,46 +1,5 @@
 import { ipcRenderer, contextBridge } from 'electron';
 
-declare global {
-  interface Window {
-    Main: typeof api;
-    ipcRenderer: typeof ipcRenderer;
-  }
-}
-
-const api = {
-  /**
-   * Here you can expose functions to the renderer process
-   * so they can interact with the main (electron) side
-   * without security problems.
-   *
-   * The function below can accessed using `window.Main.sayHello`
-   */
-  sendMessage: (message: string) => {
-    ipcRenderer.send('message', message);
-  },
-  /**
-    Here function for AppBar
-   */
-  Minimize: () => {
-    ipcRenderer.send('minimize');
-  },
-  Maximize: () => {
-    ipcRenderer.send('maximize');
-  },
-  Close: () => {
-    ipcRenderer.send('close');
-  },
-  removeLoading: () => {
-    // TO-DO
-  },
-  /**
-   * Provide an easier way to listen to events
-   */
-  on: (channel: string, callback: (data: any) => void) => {
-    ipcRenderer.on(channel, (_, data) => callback(data));
-  }
-};
-
 /**
  * Using the ipcRenderer directly in the browser through the contextBridge ist not really secure.
  * I advise using the Main/api way !!
@@ -186,10 +145,47 @@ function useLoading() {
 
 const { appendLoading, removeLoading } = useLoading();
 
-api.removeLoading = () => {
-  removeLoading();
+domReady().then(appendLoading);
+
+declare global {
+  interface Window {
+    Main: typeof api;
+    ipcRenderer: typeof ipcRenderer;
+  }
+}
+
+const api = {
+  /**
+   * Here you can expose functions to the renderer process
+   * so they can interact with the main (electron) side
+   * without security problems.
+   *
+   * The function below can accessed using `window.Main.sayHello`
+   */
+  sendMessage: (message: string) => {
+    ipcRenderer.send('message', message);
+  },
+  /**
+    Here function for AppBar
+   */
+  Minimize: () => {
+    ipcRenderer.send('minimize');
+  },
+  Maximize: () => {
+    ipcRenderer.send('maximize');
+  },
+  Close: () => {
+    ipcRenderer.send('close');
+  },
+  removeLoading: () => {
+    removeLoading();
+  },
+  /**
+   * Provide an easier way to listen to events
+   */
+  on: (channel: string, callback: (data: any) => void) => {
+    ipcRenderer.on(channel, (_, data) => callback(data));
+  }
 };
 
 contextBridge.exposeInMainWorld('Main', api);
-
-domReady().then(appendLoading);
